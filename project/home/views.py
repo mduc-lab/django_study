ccad279a761413d69f85ea5011ad17206f7fd73e
mduc-lab <nmduc14888@gmail.com>
2025-03-01 11:35:57 +0900

    add home page

---
+++ project/home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here:
+
+def index(request):
+    response = HttpResponse()
+    response.writelines("<h1>Home Page</h1>")     # This is a simple way to write HTML code in Django
+    response.write("<p>Welcome to the Home Page of the Django Study Project</p>")
+    response.write("Nguyen Minh Duc")
+    return response
