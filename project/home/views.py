3be77f444acd690a321774ac9bb02827ac038d6a
mduc-lab <nmduc14888@gmail.com>
2025-03-04 23:40:27 +0900

    update

--- project/home/views.py
+++ project/home/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 # Create your views here:
 
 def index(request):
-    response = HttpResponse()
-    response.writelines("<h1>Home Page</h1>")     # This is a simple way to write HTML code in Django
-    response.write("<p>Welcome to the Home Page of the Django Study Project</p>")
-    response.write("Nguyen Minh Duc")
-    return response
+    # response = HttpResponse()
+    # response.writelines("<h1>Home Page</h1>")     # This is a simple way to write HTML code in Django
+    # response.write("<p>Welcome to the Home Page of the Django Study Project</p>")
+    # response.write("Nguyen Minh Duc")
+    # return response
+    return render(request, 'pages/home.html')
+    # return HttpResponse("<h1>Home Page</h1><p>Welcome to the Home Page of the Django Study Project</p>")
--- project/project/settings.py
+++ project/project/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'project',
+#         'USER':'root',
+#         'PASSWORD':'',
+#         'HOST':'127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
